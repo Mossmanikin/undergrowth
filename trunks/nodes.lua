0c95ddbfde8d737d3adb738f17c1a8f9b86aed0c
Mossmanikin <Mossmanikin@gmx.de>
2013-10-02 12:02:44 +0200

    Twigs blocks (nodes), lot of new twig recipes
    Copied nodes, recipes and textures from the prehistoric mod (woodstuff)
    so all these twigs are less useless.

--- trunks/nodes.lua
+++ trunks/nodes.lua
@@ -79,3 +79,199 @@ minetest.register_node("trunks:moss_fungus", {
 	groups = {dig_immediate=2,attached_node=1},
 	sounds = default.node_sound_leaves_defaults(),
 })
+
+-----------------------------------------------------------------------------------------------
+-- TWiGS BLoCK
+-----------------------------------------------------------------------------------------------
+minetest.register_alias("woodstuff:twigs",	"trunks:twigs")
+
+minetest.register_node("trunks:twigs", {
+	description = "Twigs Block",
+	paramtype2 = "facedir",
+	tiles = {"trunks_twigs.png"},
+	groups = {choppy=2,oddly_breakable_by_hand=2,flammable=3,wood=1},
+	sounds = default.node_sound_wood_defaults(),
+})
+
+-----------------------------------------------------------------------------------------------
+-- TWiGS SLaB
+-----------------------------------------------------------------------------------------------
+minetest.register_alias("woodstuff:twigs_slab",	"trunks:twigs_slab")
+
+minetest.register_node("trunks:twigs_slab", {
+	description = "Twigs Slab",
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	tiles = {"trunks_twigs.png"},
+	node_box = {
+		type = "fixed",
+		fixed = {-1/2, -1/2, -1/2, 1/2, 0, 1/2},
+	},
+	groups = {choppy=2,oddly_breakable_by_hand=2,flammable=3,wood=1},
+	sounds = default.node_sound_wood_defaults(),
+})
+
+-----------------------------------------------------------------------------------------------
+-- TWiGS RooF
+-----------------------------------------------------------------------------------------------
+minetest.register_alias("woodstuff:twigs_roof",	"trunks:twigs_roof")
+
+minetest.register_node("trunks:twigs_roof", {
+	description = "Twigs Roof",
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	tiles = {"trunks_twigs.png"},
+	node_box = {
+		type = "fixed",
+--			{ left, bottom, front, right, top, back }
+		fixed = {
+			{-1/2, 0, 0, 1/2, 1/2, 1/2},
+			{-1/2, -1/2, -1/2, 1/2, 0, 0},
+		}
+	},
+	groups = {choppy=2,oddly_breakable_by_hand=2,flammable=3,wood=1},
+	sounds = default.node_sound_wood_defaults(),
+})
+
+-----------------------------------------------------------------------------------------------
+-- TWiGS RooF CoRNeR
+-----------------------------------------------------------------------------------------------
+minetest.register_alias("woodstuff:twigs_roof_corner",	"trunks:twigs_roof_corner")
+
+minetest.register_node("trunks:twigs_roof_corner", {
+	description = "Twigs Roof Corner",
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	tiles = {
+		"trunks_twigs_corner.png",
+		"trunks_twigs_corner.png",
+		"trunks_twigs.png"
+	},
+	node_box = {
+		type = "fixed",
+--			{ left, bottom, front, right, top, back }
+		fixed = {
+			{-1/2, 0, 0, 0, 1/2, 1/2},
+			{0, -1/2, 0, 1/2, 0, 1/2},
+			{-1/2, -1/2, -1/2, 0, 0, 0},
+		}
+	},
+	groups = {choppy=2,oddly_breakable_by_hand=2,flammable=3,wood=1},
+	sounds = default.node_sound_wood_defaults(),
+})
+
+-----------------------------------------------------------------------------------------------
+-- TWiGS RooF CoRNeR 2
+-----------------------------------------------------------------------------------------------
+minetest.register_alias("woodstuff:twigs_roof_corner_2",	"trunks:twigs_roof_corner_2")
+
+minetest.register_node("trunks:twigs_roof_corner_2", {
+	description = "Twigs Roof Corner 2",
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	tiles = {
+		"trunks_twigs_corner.png",
+		"trunks_twigs_corner.png",
+		"trunks_twigs.png"
+	},
+	node_box = {
+		type = "fixed",
+--			{ left, bottom, front, right, top, back }
+		fixed = {
+			{-1/2, -1/2, 0, 0, 0, 1/2},
+			{0, 0, 0, 1/2, 1/2, 1/2},
+			{-1/2, 0, -1/2, 0, 1/2, 0},
+		}
+	},
+	groups = {choppy=2,oddly_breakable_by_hand=2,flammable=3,wood=1},
+	sounds = default.node_sound_wood_defaults(),
+})
+
+if Auto_Roof_Corner == true then
+
+	local roof = "trunks:twigs_roof"
+	local corner = "trunks:twigs_roof_corner"
+	local corner_2 = "trunks:twigs_roof_corner_2"
+
+	minetest.register_abm({
+		nodenames = {roof},
+		interval = 1,
+		chance = 1,
+		action = function(pos)
+	
+			local node_east = 			minetest.get_node({x=pos.x+1, y=pos.y, z=pos.z  })
+			local node_west = 			minetest.get_node({x=pos.x-1, y=pos.y, z=pos.z  })
+			local node_north = 			minetest.get_node({x=pos.x,   y=pos.y, z=pos.z+1})
+			local node_south = 			minetest.get_node({x=pos.x,   y=pos.y, z=pos.z-1})
+		-- corner 1
+			if ((node_west.name == roof and node_west.param2 == 0)
+			or (node_west.name == corner and node_west.param2 == 1))
+			and ((node_north.name == roof and node_north.param2 == 3)
+			or (node_north.name == corner and node_north.param2 == 3))
+			then
+				minetest.add_node(pos, {name=corner, param2=0})
+			end
+		
+			if ((node_north.name == roof and node_north.param2 == 1)
+			or (node_north.name == corner and node_north.param2 == 2))
+			and ((node_east.name == roof and node_east.param2 == 0)
+			or (node_east.name == corner and node_east.param2 == 0))
+			then
+				minetest.add_node(pos, {name=corner, param2=1})
+			end
+		
+			if ((node_east.name == roof and node_east.param2 == 2)
+			or (node_east.name == corner and node_east.param2 == 3))
+			and ((node_south.name == roof and node_south.param2 == 1)
+			or (node_south.name == corner and node_south.param2 == 1))
+			then
+				minetest.add_node(pos, {name=corner, param2=2})
+			end
+		
+			if ((node_south.name == roof and node_south.param2 == 3)
+			or (node_south.name == corner and node_south.param2 == 0))
+			and ((node_west.name == roof and node_west.param2 == 2)
+			or (node_west.name == corner and node_west.param2 == 2))
+			then
+				minetest.add_node(pos, {name=corner, param2=3})
+			end
+		-- corner 2
+			if ((node_west.name == roof and node_west.param2 == 2)
+			or (node_west.name == corner_2 and node_west.param2 == 1))
+			and ((node_north.name == roof and node_north.param2 == 1)
+			or (node_north.name == corner_2 and node_north.param2 == 3))
+			then
+				minetest.add_node(pos, {name=corner_2, param2=0})
+			end
+		
+			if ((node_north.name == roof and node_north.param2 == 3)
+			or (node_north.name == corner_2 and node_north.param2 == 2))
+			and ((node_east.name == roof and node_east.param2 == 2)
+			or (node_east.name == corner_2 and node_east.param2 == 0))
+			then
+				minetest.add_node(pos, {name=corner_2, param2=1})
+			end
+		
+			if ((node_east.name == roof and node_east.param2 == 0)
+			or (node_east.name == corner_2 and node_east.param2 == 3))
+			and ((node_south.name == roof and node_south.param2 == 3)
+			or (node_south.name == corner_2 and node_south.param2 == 1))
+			then
+				minetest.add_node(pos, {name=corner_2, param2=2})
+			end
+		
+			if ((node_south.name == roof and node_south.param2 == 1)
+			or (node_south.name == corner_2 and node_south.param2 == 0))
+			and ((node_west.name == roof and node_west.param2 == 0)
+			or (node_west.name == corner_2 and node_west.param2 == 2))
+			then
+				minetest.add_node(pos, {name=corner_2, param2=3})
+			end
+
+		end,
+	})
+end
