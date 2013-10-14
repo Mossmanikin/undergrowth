0060be5bcd9f3f0d38079f4b90cbf44be9f54433
Mossmanikin <Mossmanikin@gmx.de>
2013-10-14 21:34:59 +0200

    Removed what's been merged to plantlife
    Textures are still there in the "old" folder

--- along_shore/init.lua
+++ along_shore/init.lua
@@ -1,14 +1,8 @@
 -----------------------------------------------------------------------------------------------
 local title		= "Along the Shore"
-local version 	= "0.0.3"
+local version 	= "0.0.4"
 local mname		= "along_shore"
 -----------------------------------------------------------------------------------------------
-
-abstract_along_shore = {}
-
-dofile(minetest.get_modpath("along_shore").."/along_shore_settings.txt")
-dofile(minetest.get_modpath("along_shore").."/generating.lua")
-dofile(minetest.get_modpath("along_shore").."/nodes.lua")
 
 -----------------------------------------------------------------------------------------------
 print("[Mod] "..title.." ["..version.."] ["..mname.."] Loaded...")
